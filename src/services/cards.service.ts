7b6468b43d63382178402be67a5eb7a6286693b9
Beta Toon <betatoon@outlook.com>
2021-01-17 16:29:51 +0100

    form get cards

--- src/services/cards.service.ts
+++ src/services/cards.service.ts
@@ -6,8 +6,8 @@ import { CardContainerModel } from "src/models/card-container.model";
 export class CardsService {
     constructor(private http: HttpClient) {}
 
-    getCard(name: string){
-        return this.http.get<CardContainerModel>('https://db.ygoprodeck.com/api/v7/cardinfo.php', {params:{name: name}});
+    getCards(name: string){
+        return this.http.get<CardContainerModel>('https://db.ygoprodeck.com/api/v7/cardinfo.php', {params:{fname: name}});
     }
   }
   
